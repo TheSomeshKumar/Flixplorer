befded101cafc33256ca01ddf206db7fea630217
thesomeshkumar <thatsomeshkumar@gmail.com>
2024-05-12 16:23:09 +0530

    bumped libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
     packaging {
         resources {
