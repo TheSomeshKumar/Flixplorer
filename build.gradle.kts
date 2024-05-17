7ff8c62f69fd26e146b0ad7a7b3ea22dae6a53fb
thesomeshkumar <thatsomeshkumar@gmail.com>
2024-05-17 18:59:33 +0530

    Added pre-commit script for detekt check

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,4 +24,27 @@ subprojects {
     }
 }
 
+// ☝️ on top here your other content of build.gradle.kts file
+
+tasks.register("copyGitHooks", Copy::class.java) {
+    description = "Copies the git hooks from /git-hooks to the .git folder."
+    group = "git hooks"
+    from("$rootDir/scripts/pre-commit")
+    into("$rootDir/.git/hooks/")
+}
+tasks.register("installGitHooks", Exec::class.java) {
+    description = "Installs the pre-commit git hooks from /git-hooks."
+    group = "git hooks"
+    workingDir = rootDir
+    commandLine = listOf("chmod")
+    args("-R", "+x", ".git/hooks/")
+    dependsOn("copyGitHooks")
+    doLast {
+        logger.info("Git hook installed successfully.")
+    }
+}
+
+afterEvaluate {
+    tasks.getByPath(":app:preBuild").dependsOn(":installGitHooks")
+}
 true // Needed to make the Suppress annotation work for the plugins block
